bedcc7543de2434ad3a0267a4c9c7f198ca7da15
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-09-19 21:27:25 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+games { 'rdr3' }
+
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+version '1.0'
+client_scripts {
+'main.lua'
+}
+
